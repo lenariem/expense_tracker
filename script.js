1aee49708946750e52c2c6bb0277fc3c3bc735a6
unknown <olena.denysova88@gmail.com>
2021-07-27 12:34:04 +0200

    add and delete transactions

--- script.js
+++ script.js
@@ -15,6 +15,34 @@ const  dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+// Add transaction
+function addTransaction(e) {
+    e.preventDefault();
+
+    if(text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add a text and amount!');
+    } else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value
+        };
+
+        transactions.push(transaction);
+        addTransactionDOM(transaction);
+        updateValues();
+
+        //reset fields after updating
+        text.value = '';
+        amount.value = '';
+    }
+}
+
+// Generate random ID 
+function generateID() {
+    return Math.floor(Math.random() * 1000000000);
+}
+
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
     //Get sign
@@ -26,7 +54,7 @@ function addTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
     `;
 
     list.appendChild(item);
@@ -43,6 +71,7 @@ function updateValues() {
                     .filter(item => item > 0)
                     .reduce((acc, item) => (acc += item), 0)
                     .toFixed(2);
+
     const expense = (amounts
                     .filter(item => item < 0)
                     .reduce((acc, item) => (acc += item), 0) * -1)
@@ -51,7 +80,13 @@ function updateValues() {
     balance.innerText = `€${total}`;
     money_plus.innerText = `€${income}`;
     money_minus.innerText = `€${expense}`;
+}
 
+// Remove transaction by ID
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    init();
 }
 
 
@@ -62,5 +97,6 @@ function init() {
     transactions.forEach(addTransactionDOM);
     updateValues();
 }
-
 init();
+
+form.addEventListener('submit', addTransaction);
