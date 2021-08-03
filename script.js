b673de4a1b6bfcfc4a36ac7344de666dd084b464
unknown <olena.denysova88@gmail.com>
2021-08-03 11:05:22 +0200

    btns to transsactions

--- script.js
+++ script.js
@@ -73,7 +73,10 @@ function addTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+        <button class="btn-icon delete" onclick="removeTransaction(${transaction.id})">x</button>
+        <button class="btn-icon edit" title="edit">&#9998;</button>
+        <button class="btn-icon save" title="save">&#10004;</button>
     `;
 
     list.appendChild(item);
